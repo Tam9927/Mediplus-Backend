e54b3b319f779c745a8582760140dba6107b0740
Tam9927 <bsse1127@iit.du.ac.bd>
2024-12-15 19:27:30 +0600

    integrate swagger documentaion in all APIs

--- src/test-request/test-request.controller.ts
+++ src/test-request/test-request.controller.ts
@@ -1,46 +1,4 @@
 
-
-// import { Controller, Post, Body, Get, Param, Patch } from '@nestjs/common';
-// import { BadRequestException } from '@nestjs/common';
-// import { TestRequestService } from './test-request.service';
-// import { ValidationPipe } from '@nestjs/common';
-// import { TestRequestDTO } from 'src/test-request/dto/test-request.dto';
-
-// @Controller('test-requests')
-// export class TestRequestController {
-//   constructor(private readonly testRequestService: TestRequestService) {}
-
-//   @Post()
-//   async createTestRequest(@Body(new ValidationPipe()) testRequestDTO: TestRequestDTO) {
-//     return await this.testRequestService.createRequest(testRequestDTO);
-//   }
-
-//   @Get(':id')
-//   async getRequestById(@Param('id') id: number) {
-//     return await this.testRequestService.getRequestById(id);
-//   }
-
-//   @Get()
-//   async getAllRequests() {
-//     return await this.testRequestService.getAllRequests();  
-//   }
-
-//   @Patch(':id/process-payment')
-//   async processPayment(
-//     @Param('id') id: number,  
-//     @Body('paymentAmount') paymentAmount: number, // Retrieve payment amount from request body
-//   ) {
-//     if (paymentAmount <= 0) {
-//       throw new BadRequestException('Payment amount must be greater than zero');
-//     }
-//     return await this.testRequestService.processPayment(id, paymentAmount);
-//   }
-  
-
-// }
-
-
-
 import {
   Controller,
   Post,
--- src/test/dto/test.dto.ts
+++ src/test/dto/test.dto.ts
@@ -1,19 +1,40 @@
-import { IsString, IsNotEmpty, IsInt, IsOptional, IsNumber,IsPositive } from 'class-validator';
+import { IsString, IsNotEmpty, IsInt, IsOptional, IsNumber, IsPositive } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class TestDTO {
+  @ApiProperty({
+    description: 'Name of the test',
+    example: 'Complete Blood Count',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'ID of the test category to which this test belongs',
+    example: 1,
+    required: true,
+  })
   @IsInt()
-  testCategoryId: number;  // Foreign key to TestCategory
+  testCategoryId: number;
 
+  @ApiProperty({
+    description: 'Optional description of the test',
+    example: 'A test to measure various components of the blood',
+    required: false,
+  })
   @IsOptional()
-  description?: string;  // Optional description for the test
+  @IsString()
+  description?: string;
 
+  @ApiProperty({
+    description: 'Cost of the test, must be a positive number',
+    example: 500,
+    required: true,
+  })
   @IsNumber()
   @IsPositive()
   cost: number;
-
-
 }
+
